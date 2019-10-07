7b1d49d796fe218b3cba25981c369266dfb90f29
Manuel Riquelme <clonbg@gmail.com>
2019-10-07 15:33:56 +0200

    diccionarios

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Curso_python_pildoras_informaticas
 - Video 9 min 11:30
+- Video 10
--- diccionarios.py
+++ diccionarios.py
@@ -1,6 +1,6 @@
 miDiccionario={
 	"nombre": "Manuel",
-	"dni": "22222222"
+	"dni": 22222222
 }
 print(miDiccionario)
 print(miDiccionario["nombre"])
@@ -11,3 +11,22 @@ print(miDiccionario)
 del miDiccionario["dni"]
 print(miDiccionario)
 
+miDiccionario2={
+	23:"Jordan",
+	"Nombre":"Michael",
+	"Equipo":"Chicago",
+	"anillos":[1991,1992,1993,1996,1997,1998]
+}
+print(miDiccionario2["anillos"])
+
+miDiccionario2={
+	23:"Jordan",
+	"Nombre":"Michael",
+	"Equipo":"Chicago",
+	"anillos":{"temporadas":[1991,1992,1993,1996,1997,1998]}
+}
+print(miDiccionario2["anillos"])
+print(miDiccionario2.keys())
+print(miDiccionario2.values())
+print(len(miDiccionario2))
+print(len(miDiccionario2["anillos"]))
